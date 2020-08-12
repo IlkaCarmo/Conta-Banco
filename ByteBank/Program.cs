625f59d3bb1a958e2cae992fc691dece891328d1
IlkaCarmo <ilka_2009@ig.com.br>
2020-08-11 21:40:32 -0300

    first commit

---
+++ ByteBank/Program.cs
@@ -0,0 +1,49 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+
+            Funcionario carlos = new Funcionario("546.879.157-20");
+
+            carlos.Nome = "Carlos";
+            carlos.Salario = 2000;
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor("565.996.741 - 20");
+            roberta.Nome = "Roberta";
+            roberta.Salario = 5000;
+
+            Funcionario robertaTeste = roberta;
+
+            Console.WriteLine("bonificação referencia direto:" + roberta.GetBonificacao());
+
+            Console.WriteLine("bonificação referencia funcionario:" + robertaTeste.GetBonificacao());
+
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+            Console.WriteLine(carlos.GetBonificacao());
+
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total bonificação:"+gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
